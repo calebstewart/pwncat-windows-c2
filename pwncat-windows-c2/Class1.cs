fd1f5375fddb4f9cbebfeb4490eb5a67ae889253
Caleb Stewart <caleb.stewart94@gmail.com>
2021-01-09 18:05:49 -0800

    Add project files.

---
+++ pwncat-windows-c2/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pwncat_windows_c2
+{
+    public class Class1
+    {
+    }
+}
